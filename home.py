7617f82c6959037101def3a82cf40aef5bf2318b
Francisco Quiñonez <Mr.qnns008@gmail.com>
2024-04-09 00:03:32 -0500

    first commit

---
+++ home.py
@@ -0,0 +1,27 @@
+# Comienzo 8/04/2024
+
+def menu():
+    
+    tip = str(input('''Tips Menu ☺ Bob Ross:
+    \t 1| Learn to draw
+    \t 2| Phrase
+    \t 3| Exit 
+Select: '''))
+
+    if tip== "1":
+        print("'how to draw better' \n the artist: drawing")
+    elif tip == "2":
+        print("We don't make mistakes, just happy accidents and we learn to work with them.")
+    elif tip=="3":
+        exit
+    else:
+        menu()    
+    return  tip
+
+menu()
+
+
+# JAJAJA
+# programar_se_aprende_programando_programar-ir-donde-las-cariñosas ha, ha, ha
+# C:\Users\OPTIPLEX\docs\py_practica
+# py home.py <Run Terminal >
