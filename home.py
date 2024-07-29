3582fee35f60950fa5c1efd962ee46bffd6524f1
Francisco Quiñonez <136272232+frankqv@users.noreply.github.com>
2024-07-28 22:36:23 -0500

    Upadate Just Do It.py
    28/07/2024

--- home.py
+++ home.py
@@ -1,27 +1,35 @@
-# Comienzo 8/04/2024
+#### Comienzo 8/04/2024
+#### 28/07/2024
 
 def menu():
     
-    tip = str(input('''Tips Menu ☺ Bob Ross:
+    opcion = str(input('''Tips Menu ☺ Bob Ross:
     \t 1| Learn to draw
     \t 2| Phrase
     \t 3| Exit 
+    \t 4| 2 Textos.
 Select: '''))
 
-    if tip== "1":
+    if opcion == "1":
         print("'how to draw better' \n the artist: drawing")
-    elif tip == "2":
+    elif opcion == "2":
         print("We don't make mistakes, just happy accidents and we learn to work with them.")
-    elif tip=="3":
+    elif opcion =="3":
         exit
+
+    elif opcion == "4":
+        a = input("text_1:   ")
+        b = input("text_2:   ")
+   
+        
+        menu()
     else:
-        menu()    
-    return  tip
+        menu()
+        print("Dato invalido")
+    return  opcion
 
 menu()
 
 
-# JAJAJA
-# programar_se_aprende_programando_programar-ir-donde-las-cariñosas ha, ha, ha
 # C:\Users\OPTIPLEX\docs\py_practica
 # py home.py <Run Terminal >
